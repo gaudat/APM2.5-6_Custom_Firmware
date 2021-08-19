7d9d1f5bad97e147838b74ca4fd3f8d06cb9b72b
gaudat <me@gaudat.name>
2021-08-19 18:28:52 +0800

    DMP does not work

--- APM2.5-6_Custom_Firmware.ino
+++ APM2.5-6_Custom_Firmware.ino
@@ -1,4 +1,7 @@
 #include <SPI.h>
+
+#include "MPU6000.h"
+
 #include "APM_PPM.h"
 #include "APM2RCOutput.h"
 
@@ -9,122 +12,122 @@ const int ledRed = 27;
 
 //SPI Bus SlaveSelect pins
 const int imuSelect = 53;
-const int pressureSelect = 40;  
+const int pressureSelect = 40;
 
 /*
-_______________DataFlash(AT45DB161-MU) Pinout_______________
-PJ1 = MOSI_DF = 14
-PJ2 = SCK_DF = NA
-PA6 = CS_DF = 28
-PG0 = RST_DEF = 41
-I assume these pins go to some SPI hardware...
+  _______________DataFlash(AT45DB161-MU) Pinout_______________
+  PJ1 = MOSI_DF = 14
+  PJ2 = SCK_DF = NA
+  PA6 = CS_DF = 28
+  PG0 = RST_DEF = 41
+  I assume these pins go to some SPI hardware...
 */
 
 /*
-_______________Magnetometer(HMC5883L) Pinout________________
-SCL = PD0 = 21
-SDA = PD1 = 20
-This seems like the defualt bus
+  _______________Magnetometer(HMC5883L) Pinout________________
+  SCL = PD0 = 21
+  SDA = PD1 = 20
+  This seems like the defualt bus
 */
 
 /*
-_______________Servo PPM Encoder Pinout______________________
-Definitely a lot more going on in the circuit than I orignally thought.
-ATMEGA32U4  ATMEGA2560
-PPM_PD1      PA2
-PPM_PD0      PA1
+  _______________Servo PPM Encoder Pinout______________________
+  Definitely a lot more going on in the circuit than I orignally thought.
+  ATMEGA32U4  ATMEGA2560
+  PPM_PD1      PA2
+  PPM_PD0      PA1
 
-PPM_PC2 => Throws switch for whole system
-This pin is on the ATMEGA32U4
+  PPM_PC2 => Throws switch for whole system
+  This pin is on the ATMEGA32U4
 
-This will throw the connections for pins
+  This will throw the connections for pins
 
-RX0-1                     ____PPM_TX (Normally Open)        
-ATMEGA2560_PE0 (RX?)_____|____3DR_RX (Normally Closed)
+  RX0-1                     ____PPM_TX (Normally Open)
+  ATMEGA2560_PE0 (RX?)_____|____3DR_RX (Normally Closed)
 
-TX0-O                     ____PPM_RX (Normally Open)
-ATMEGA2560_PE1 (TX?)_____|____3DR_TX (Normally Closed)
+  TX0-O                     ____PPM_RX (Normally Open)
+  ATMEGA2560_PE1 (TX?)_____|____3DR_TX (Normally Closed)
 */
 
 APM_PPM rxin;
 
 /*
-_______________Extra IO_______________________________________
-IO1 = PF0 = ADC0
-IO2 = PF1 = ADC1
-IO3 = PF2 = ADC2
-IO4 = PF3 = ADC3
-IO5 = PF4 = ADC4
-IO6 = PF5 = ADC5
-IO7 = PF6 = ADC6
-IO8 = PF8 = ADC7
-IO9 = PK0 = ADC8
+  _______________Extra IO_______________________________________
+  IO1 = PF0 = ADC0
+  IO2 = PF1 = ADC1
+  IO3 = PF2 = ADC2
+  IO4 = PF3 = ADC3
+  IO5 = PF4 = ADC4
+  IO6 = PF5 = ADC5
+  IO7 = PF6 = ADC6
+  IO8 = PF8 = ADC7
+  IO9 = PK0 = ADC8
 */
 const int io_count = 9;
 const int io_pins[9] = {A0, A1, A2, A3, A4, A5, A6, A7, A8};
 
 class SlewRateLimiter {
   public:
-  SlewRateLimiter(int limit_per_tick, bool limit_accel_only) : 
-  neutral(0),
-  first_run(false),
-  limit_per_tick(limit_per_tick),
-  limit_accel_only(limit_accel_only)
-   {
+    SlewRateLimiter(int limit_per_tick, bool limit_accel_only) :
+      neutral(0),
+      first_run(false),
+      limit_per_tick(limit_per_tick),
+      limit_accel_only(limit_accel_only)
+    {
 
-  }
-  int run(int input, long ticks) {
-    if (first_run) {
+    }
+    int run(int input, long ticks) {
+      if (first_run) {
+        last_ticks = ticks;
+        last_output = input;
+        first_run = false;
+        return input;
+      }
+      long time_diff = ticks - last_ticks;
       last_ticks = ticks;
-      last_output = input;
-      first_run = false;
-      return input;
-    }
-    long time_diff = ticks - last_ticks;
-    last_ticks = ticks;
-    // See if accel or decel
-    if (last_output >= neutral && input > last_output) {
-      // Forward Accel 
-      int upper_limit = last_output + time_diff * limit_per_tick;
-      input = constrain(input, neutral, upper_limit);
-    }
-    else if (last_output >= neutral && input <= last_output) {
-      // Forward Decel
-      if (!limit_accel_only) {
-        int lower_limit = last_output - time_diff * limit_per_tick;
-        // Prevent output from going under neutral
-        if (lower_limit < neutral) {
-          lower_limit = neutral;
-        }
-        input = constrain(input, lower_limit, last_output);
-      }
-    }
-    else if (last_output < neutral && input < last_output) {
-      // Backward Accel
-      int lower_limit = last_output - time_diff * limit_per_tick;
-      input = constrain(input, lower_limit, neutral);
-    }
-    else if (last_output < neutral && input >= last_output) {
-      // Backward Decel
-      if (!limit_accel_only) {
+      // See if accel or decel
+      if (last_output >= neutral && input > last_output) {
+        // Forward Accel
         int upper_limit = last_output + time_diff * limit_per_tick;
-        // Prevent output from going above neutral
-        if (upper_limit > neutral) {
-          upper_limit = neutral;
-        }
-        input = constrain(input, last_output, upper_limit);
+        input = constrain(input, neutral, upper_limit);
       }
+      else if (last_output >= neutral && input <= last_output) {
+        // Forward Decel
+        if (!limit_accel_only) {
+          int lower_limit = last_output - time_diff * limit_per_tick;
+          // Prevent output from going under neutral
+          if (lower_limit < neutral) {
+            lower_limit = neutral;
+          }
+          input = constrain(input, lower_limit, last_output);
+        }
+      }
+      else if (last_output < neutral && input < last_output) {
+        // Backward Accel
+        int lower_limit = last_output - time_diff * limit_per_tick;
+        input = constrain(input, lower_limit, neutral);
+      }
+      else if (last_output < neutral && input >= last_output) {
+        // Backward Decel
+        if (!limit_accel_only) {
+          int upper_limit = last_output + time_diff * limit_per_tick;
+          // Prevent output from going above neutral
+          if (upper_limit > neutral) {
+            upper_limit = neutral;
+          }
+          input = constrain(input, last_output, upper_limit);
+        }
+      }
+      last_output = input;
+      return last_output;
     }
-    last_output = input;
-    return last_output;
-  }
   private:
-  int neutral;
-  long last_ticks;
-  int last_output;
-  bool first_run;
-  int limit_per_tick;
-  bool limit_accel_only;
+    int neutral;
+    long last_ticks;
+    int last_output;
+    bool first_run;
+    int limit_per_tick;
+    bool limit_accel_only;
 };
 
 SlewRateLimiter throttle_SLM(1, true);
@@ -134,7 +137,7 @@ void motor_loop() {
   // Front = +y (Ch 3), Right = +x (Ch 1)
   const int front_ch = 2;
   const int right_ch = 0;
-  
+
   const int neutral = 1500;
   const int input_deadzone = 50;
 
@@ -169,42 +172,49 @@ void motor_loop() {
   // Write to ouput
   if (oe) {
 
-  output.write(0, motor_left);
-  output.write(1, motor_right);
+    output.write(0, motor_left);
+    output.write(1, motor_right);
   } else {
-        
-  output.write(0, neutral);
-  output.write(1, neutral);
+
+    output.write(0, neutral);
+    output.write(1, neutral);
   }
 }
 
+//SPI Bus SlaveSelect pins
+MPU6000 imu;
+float accelVec[3];
+float gyroVec[3];
+
+#define X 0
+#define Y 1
+#define Z 2
+
 void setup () {
-  
+
   Serial.begin(115200);
-  
+
   pinMode(ledBlue, OUTPUT);
   pinMode(ledYellow, OUTPUT);
   pinMode(ledRed, OUTPUT);
-  
+
   digitalWrite(ledBlue, HIGH);
   digitalWrite(ledYellow, HIGH);
   digitalWrite(ledRed, HIGH);
-  
+
   pinMode(imuSelect, OUTPUT);
   pinMode(pressureSelect, OUTPUT);
-  
+
   digitalWrite(imuSelect, HIGH);
   digitalWrite(pressureSelect, HIGH);
-  
-  SPI.begin();
-  SPI.setBitOrder(MSBFIRST);
-  SPI.setClockDivider(SPI_CLOCK_DIV16);
-  SPI.setDataMode(SPI_MODE3);
-  delay(100);
 
+  imu.initialize();
+  imu.CalibrateGyro(6);
+  imu.CalibrateAccel(6);
+  
   output.init();
   for (int i = 0; i < 8; i++) {
-  output.enable_ch(i);
+    output.enable_ch(i);
   }
 
   rxin.initialize();
@@ -212,14 +222,15 @@ void setup () {
 
 void servos_mirror() {
   uint16_t outputs[8];
-  
+
   rxin.read(outputs);
   output.write(0, outputs, 8);
 }
 
 void loop() {
   digitalWrite(ledRed, !digitalRead(ledRed));
-  motor_loop();
+  //motor_loop();
+  loop_spi_print();
 }
 
 void loop_ppm_pass() {
@@ -230,7 +241,7 @@ void loop_ppm_print () {
   Serial.println(rxin.newData());
   uint16_t val[numChannel];
   rxin.read(val);
-  for (int ch=0; ch < numChannel; ch++) {
+  for (int ch = 0; ch < numChannel; ch++) {
     Serial.print(val[ch]);
     Serial.print(" ");
   }
@@ -239,11 +250,14 @@ void loop_ppm_print () {
 }
 
 void loop_spi_print () {
-  
-  byte resultat = spi_read(0x3B | 0b10000000);
-  Serial.println(resultat, BIN);
-  delay(1000);
-
+  int16_t dofs[6];
+  dofs[0] = imu.readWord(0x3B);
+  dofs[1] = imu.readWord(0x3D);
+  dofs[2] = imu.readWord(0x3F);
+  dofs[3] = imu.readWord(0x43);
+  dofs[4] = imu.readWord(0x45);
+  dofs[5] = imu.readWord(0x47);
+  Serial.write((uint8_t *)dofs, 12);
 }
 
 byte spi_read(byte read_command) {
---
+++ MPU6000.cpp
@@ -0,0 +1,355 @@
+#include "MPU6000.h"
+
+//Class constructor
+MPU6000::MPU6000(){
+}
+
+void MPU6000::initialize(){
+
+  //De-select all devices on SPI bus
+  pinMode(imuSelect, OUTPUT);
+  pinMode(pressureSelect, OUTPUT);
+  digitalWrite(imuSelect, HIGH);
+  digitalWrite(pressureSelect, HIGH);
+
+  //Initialize SPI Bus
+  SPI.begin();
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setClockDivider(SPI_CLOCK_DIV16); //Set to 1 Mhz
+  SPI.setDataMode(SPI_MODE3);
+
+  writeReg(0x6B, 0x80);
+  delay(100);
+  writeReg(0x6A, 0x07);
+  delay(100);
+  writeReg(0x6B, 0x01);
+  writeReg(0x38, 0x00);
+  writeReg(0x23, 0x00);
+  writeReg(0x1C, 0x00);
+  writeReg(0x37, 0x80);
+  writeReg(0x6B, 0x01);
+  writeReg(0x19, 0x04);
+  writeReg(0x1A, 0x03);
+  DEBUG_PRINTLN("DMP WRITE START");
+  digitalWrite(imuSelect, HIGH);
+  for (uint16_t offset = 0; offset < MPU6050_DMP_CODE_SIZE; offset++) {
+    if ((offset & 0x00ff) == 0) {
+      digitalWrite(imuSelect, HIGH);
+      // Change banks
+      digitalWrite(25, !digitalRead(25));
+      delay(100);
+      writeReg(0x6D, offset >> 8);
+      writeReg(0x6E, 0x00);
+      digitalWrite(imuSelect, LOW);
+      SPI.transfer(0x6F);
+    }
+    SPI.transfer(dmpMemory[offset]);
+  }
+  digitalWrite(imuSelect, HIGH);
+  for (uint16_t offset = 0; offset < MPU6050_DMP_CODE_SIZE; offset++) {
+    if ((offset & 0x00ff) == 0) {
+      digitalWrite(imuSelect, HIGH);
+      // Change banks
+      digitalWrite(25, !digitalRead(25));
+      delay(100);
+      writeReg(0x6D, offset >> 8);
+      writeReg(0x6E, 0x00);
+      digitalWrite(imuSelect, LOW);
+      SPI.transfer(0x6F | 0x80);
+    }
+    uint8_t res = SPI.transfer(0x00);
+    
+    if (res != dmpMemory[offset]) {
+      DEBUG_PRINT("READBACK ");
+      DEBUG_PRINT(offset);
+      DEBUG_PRINT(" ");
+      DEBUG_PRINT(res);
+      DEBUG_PRINT(" ");
+      DEBUG_PRINT(dmpMemory[offset]);
+      DEBUG_PRINTLN(" X");
+    }
+  }
+  DEBUG_PRINTLN("DMP WRITE DONE");
+  writeWord(0x70, 0x03, 0x00);
+  writeReg(0x1B, 0x18);
+  writeReg(0x6A, 0xC0);
+  writeReg(0x38, 0x02);
+  writeReg(0x6A, 0x04);
+   gyroScale = getGyroScale();
+   accelScale = getAccelScale();
+}
+
+bool MPU6000::testConnection(){
+  uint8_t id;
+  id = readReg(MPUREG_WHOAMI);
+
+  if(id == 0x68){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+void MPU6000::readScaled(){
+  int16_t a[3], g[3];
+  readImu(a, g);
+  for(int j = 0; j<3; j++){
+    accel[j] = a[j]*accelScale;
+    gyro[j] = g[j]*gyroScale;
+  }
+  return;
+}
+
+void MPU6000::readImu(int16_t accel[3], int16_t gyro[3]){
+  uint8_t byte_H, byte_L, dump;
+
+  //Start a stream read of the sensor outputs:
+  uint8_t addr = MPUREG_ACCEL_XOUT_H | 0x80;
+
+  digitalWrite(imuSelect, LOW);
+
+  dump = SPI.transfer(addr);
+
+  // Read AccelX
+  byte_H = SPI.transfer(0);
+  byte_L = SPI.transfer(0);
+  accel[0] = ((int)byte_H << 8) | byte_L;
+
+  // Read AccelY
+  byte_H = SPI.transfer(0);
+  byte_L = SPI.transfer(0);
+  accel[1] = ((int)byte_H << 8) | byte_L;
+
+  // Read AccelZ
+  byte_H = SPI.transfer(0);
+  byte_L = SPI.transfer(0);
+  accel[2] = ((int)byte_H << 8)| byte_L;
+
+  // Read Temp
+  byte_H = SPI.transfer(0);
+  byte_L = SPI.transfer(0);
+  mpu_temp = ((int)byte_H << 8)| byte_L;
+
+  // Read GyroX
+  byte_H = SPI.transfer(0);
+  byte_L = SPI.transfer(0);
+  gyro[0] = ((int)byte_H << 8)| byte_L;
+
+  // Read GyroY
+  byte_H = SPI.transfer(0);
+  byte_L = SPI.transfer(0);
+  gyro[1] = ((int)byte_H << 8) | byte_L;
+
+  // Read GyroZ
+  byte_H = SPI.transfer(0);
+  byte_L = SPI.transfer(0);
+  gyro[2] = ((int)byte_H <<8 ) | byte_L;
+
+  digitalWrite(imuSelect, HIGH);
+}
+
+float MPU6000::getAccelScale(){
+  const int bitNum = 32768;//(2^16)/2 Internal 16bit ADC
+  uint8_t configByte;
+  configByte = readReg(MPUREG_ACCEL_CONFIG);
+  //Eliminate all bits except scale bits
+  configByte &= 0x18;
+  configByte = configByte >> 3;
+  Serial.println(configByte);
+  if (configByte == 0){
+    return(abs((float)2.0/bitNum));
+  }
+  else if (configByte == 1){
+    return(abs((float)4.0/bitNum));
+  }
+  else if (configByte == 2){
+    return(abs((float)8.0/bitNum));
+  }
+  else if (configByte == 3){
+    return(abs((float)16.0/bitNum));
+  }
+  else{
+    return ((float)-1.0);
+  }
+}
+
+float MPU6000::getGyroScale(){
+  const int bitNum = 32768;//(2^16)/2 Internal 16bit ADC
+  uint8_t configByte;
+  configByte = readReg(MPUREG_GYRO_CONFIG);
+  //Eliminate all bits except scale bits
+  configByte &= 0x18;
+  configByte = configByte >> 3;
+  Serial.println(configByte);
+  if (configByte == 0){
+    return(abs((float)250.0/bitNum));
+  }
+  else if (configByte == 1){
+    return(abs((float)500.0/bitNum));
+  }
+  else if (configByte == 2){
+    return(abs((float)1000.0/bitNum));
+  }
+  else if (configByte == 3){
+    return(abs((float)2000.0/bitNum));
+  }
+  else{
+    return ((float)-1.0);
+  }
+}
+
+uint8_t MPU6000::readReg(uint8_t reg){
+  uint8_t value;
+  digitalWrite(imuSelect, LOW);
+  SPI.transfer(reg | 0x80); //Set read bit
+  value = SPI.transfer((uint8_t) 0x00);
+  digitalWrite(imuSelect, HIGH);
+  return value;
+}
+
+void MPU6000::writeReg(uint8_t reg, uint8_t data){
+  uint8_t dump;
+
+  digitalWrite(imuSelect, LOW);
+  dump = SPI.transfer(reg);
+  dump = SPI.transfer(data);
+  digitalWrite(imuSelect, HIGH);
+}
+
+// Modified from jrowberg/i2cdevlib /Arduino/MPU6050/MPU6050.cpp
+
+int16_t MPU6000::readWord(uint8_t reg) {
+  uint8_t high, low;
+  digitalWrite(imuSelect, LOW);
+  SPI.transfer(reg | 0x80);
+  high = SPI.transfer(0);
+  low = SPI.transfer(0);
+  digitalWrite(imuSelect, HIGH);
+  int16_t out = (high << 8) | low;
+  return out;
+}
+
+void MPU6000::writeWord(uint8_t reg, uint8_t high, uint8_t low) {
+  digitalWrite(imuSelect, LOW);
+  SPI.transfer(reg);
+  SPI.transfer(high);
+  SPI.transfer(low);
+  digitalWrite(imuSelect, HIGH);
+}
+
+void MPU6000::CalibrateGyro(uint8_t Loops ) {
+  double kP = 0.3;
+  double kI = 90;
+  float x;
+  x = (100 - map(Loops, 1, 5, 20, 0)) * .01;
+  kP *= x;
+  kI *= x;
+  
+  PID( 0x43,  kP, kI,  Loops);
+}
+
+void MPU6000::CalibrateAccel(uint8_t Loops ) {
+
+	float kP = 0.3;
+	float kI = 20;
+	float x;
+	x = (100 - map(Loops, 1, 5, 20, 0)) * .01;
+	kP *= x;
+	kI *= x;
+	PID( 0x3B, kP, kI,  Loops);
+}
+
+void MPU6000::PID(uint8_t ReadAddress, float kP,float kI, uint8_t Loops){
+	uint8_t SaveAddress = (ReadAddress == 0x3B)?(0x06):0x13;
+
+	int16_t  Data;
+	float Reading;
+	int16_t BitZero[3];
+	uint8_t shift =(SaveAddress == 0x77)?3:2;
+	float Error, PTerm, ITerm[3];
+	int16_t eSample;
+	uint32_t eSum ;
+	Serial.write('>');
+	for (int i = 0; i < 3; i++) {
+		Data = readWord(SaveAddress + (i * shift)); // reads 1 or more 16 bit integers (Word)
+		Reading = Data;
+		if(SaveAddress != 0x13){
+			BitZero[i] = Data & 1;										 // Capture Bit Zero to properly handle Accelerometer calibration
+			ITerm[i] = ((float)Reading) * 8;
+			} else {
+			ITerm[i] = Reading * 4;
+		}
+	}
+	for (int L = 0; L < Loops; L++) {
+		eSample = 0;
+		for (int c = 0; c < 100; c++) {// 100 PI Calculations
+			eSum = 0;
+			for (int i = 0; i < 3; i++) {
+				Data = readWord(ReadAddress + (i * 2)); // reads 1 or more 16 bit integers (Word)
+				Reading = Data;
+				if ((ReadAddress == 0x3B)&&(i == 2)) Reading -= 16384;	//remove Gravity
+				Error = -Reading;
+				eSum += abs(Reading);
+				PTerm = kP * Error;
+				ITerm[i] += (Error * 0.001) * kI;				// Integral term 1000 Calculations a second = 0.001
+				if(SaveAddress != 0x13){
+					Data = round((PTerm + ITerm[i] ) / 8);		//Compute PID Output
+					Data = ((Data)&0xFFFE) |BitZero[i];			// Insert Bit0 Saved at beginning
+				} else Data = round((PTerm + ITerm[i] ) / 4);	//Compute PID Output
+        writeWord(SaveAddress + (i * shift), (Data >> 8) & 0xff, Data & 0xff);
+			}
+			if((c == 99) && eSum > 1000){						// Error is still to great to continue 
+				c = 0;
+				Serial.write('*');
+			}
+			if((eSum * ((ReadAddress == 0x3B)?.05: 1)) < 5) eSample++;	// Successfully found offsets prepare to  advance
+			if((eSum < 100) && (c > 10) && (eSample >= 10)) break;		// Advance to next Loop
+			delay(1);
+		}
+		Serial.write('.');
+		kP *= .75;
+		kI *= .75;
+		for (int i = 0; i < 3; i++){
+			if(SaveAddress != 0x13) {
+				Data = round((ITerm[i] ) / 8);		//Compute PID Output
+				Data = ((Data)&0xFFFE) |BitZero[i];	// Insert Bit0 Saved at beginning
+			} else Data = round((ITerm[i]) / 4);
+      writeWord(SaveAddress + (i * shift), (Data >> 8) & 0xff, Data & 0xff);
+		}
+	}
+  writeReg(0x6A, 0x0C);
+}
+
+#define printfloatx(Name,Variable,Spaces,Precision,EndTxt) { Serial.print(F(Name)); {char S[(Spaces + Precision + 3)];Serial.print(F(" ")); Serial.print(dtostrf((float)Variable,Spaces,Precision ,S));}Serial.print(F(EndTxt)); }//Name,Variable,Spaces,Precision,EndTxt
+void MPU6000::PrintActiveOffsets() {
+	uint8_t AOffsetRegister = 0x06;
+	int16_t Data[3];
+	//Serial.print(F("Offset Register 0x"));
+	//Serial.print(AOffsetRegister>>4,HEX);Serial.print(AOffsetRegister&0x0F,HEX);
+	Serial.print(F("\n//           X Accel  Y Accel  Z Accel   X Gyro   Y Gyro   Z Gyro\n//OFFSETS   "));
+	if(AOffsetRegister == 0x06)	{
+    Data[0] = readWord(AOffsetRegister);
+    Data[1] = readWord(AOffsetRegister + 2);
+    Data[2] = readWord(AOffsetRegister + 4);
+  }
+	//	A_OFFSET_H_READ_A_OFFS(Data);
+	printfloatx("", Data[0], 5, 0, ",  ");
+	printfloatx("", Data[1], 5, 0, ",  ");
+	printfloatx("", Data[2], 5, 0, ",  ");
+  Data[0] = readWord(0x13);
+  Data[1] = readWord(0x15);
+  Data[2] = readWord(0x17);
+	//	XG_OFFSET_H_READ_OFFS_USR(Data);
+	printfloatx("", Data[0], 5, 0, ",  ");
+	printfloatx("", Data[1], 5, 0, ",  ");
+	printfloatx("", Data[2], 5, 0, "\n");
+}
+
+
+bool MPU6000::getDMPEnabled() {
+  uint8_t res = readReg(0x6A);
+  return res & 0x80;
+}
+void MPU6000::setDMPEnabled(bool enabled) {
+  writeReg(0x6A, enabled ? 0xCC : 0x0C);
+}
